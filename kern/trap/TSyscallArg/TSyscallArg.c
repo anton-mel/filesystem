ac0f7ecd62a96ce67c0195835bac2cb514b04e80
Melnychuk Anton <am3785@zoo.cs.yale.edu>
2025-02-25 17:50:45 -0500

    Syscall Args [Completed]

--- kern/trap/TSyscallArg/TSyscallArg.c
+++ kern/trap/TSyscallArg/TSyscallArg.c
@@ -5,6 +5,24 @@
 
 extern tf_t uctx_pool[NUM_IDS];
 
+// copied over from trap.h:
+// uctx_pool[curid].regs format
+// typedef struct pushregs {
+//     uint32_t edi;
+//     uint32_t esi;
+//     uint32_t ebp;
+//     uint32_t oesp;  /* Useless */
+//     uint32_t ebx;
+//     uint32_t edx;
+//     uint32_t ecx;
+//     uint32_t eax;
+// } pushregs;
+
+// !NOTE: The order is defined as per the spec.
+// The system call number will go in %eax, and the 
+// arguments (up to five of them) will go in %ebx, 
+// %ecx, %edx, %esi, and %edi, respectively.
+
 /**
  * Retrieves the system call arguments from uctx_pool that get
  * passed in from the current running process' system call.
@@ -12,37 +30,43 @@ extern tf_t uctx_pool[NUM_IDS];
 unsigned int syscall_get_arg1(void)
 {
     // TODO
-    return 0;
+    unsigned int curid = get_curid();
+    return uctx_pool[curid].regs.eax;
 }
 
 unsigned int syscall_get_arg2(void)
 {
     // TODO
-    return 0;
+    unsigned int curid = get_curid();
+    return uctx_pool[curid].regs.ebx;
 }
 
 unsigned int syscall_get_arg3(void)
 {
     // TODO
-    return 0;
+    unsigned int curid = get_curid();
+    return uctx_pool[curid].regs.ecx;
 }
 
 unsigned int syscall_get_arg4(void)
 {
     // TODO
-    return 0;
+    unsigned int curid = get_curid();
+    return uctx_pool[curid].regs.edx;
 }
 
 unsigned int syscall_get_arg5(void)
 {
     // TODO
-    return 0;
+    unsigned int curid = get_curid();
+    return uctx_pool[curid].regs.esi;
 }
 
 unsigned int syscall_get_arg6(void)
 {
     // TODO
-    return 0;
+    unsigned int curid = get_curid();
+    return uctx_pool[curid].regs.edi;
 }
 
 /**
@@ -52,7 +76,22 @@ unsigned int syscall_get_arg6(void)
 void syscall_set_errno(unsigned int errno)
 {
     // TODO
+    // !NOTE: Following the spec documentation.
+    // A system call always returns with an error number 
+    // via register EAX. All valid error numbers are 
+    // listed in __error_nr defined in kern/lib/syscall.h.
+    unsigned int curid = get_curid();
+    uctx_pool[curid].regs.eax = errno;
+    // E_SUCC indicates success (no errors).
 }
+
+// !NOTE: Following the order, do the opposite here.
+// A system call can return at most 5 32-bit values 
+// via registers EBX, ECX, EDX, ESI and EDI. When the 
+// trap happens, we first save the corresponding trap 
+// frame (the register values of the user process) into 
+// memory (uctx_pool), and we restores the register 
+// values based on the saved ones.
 
 /**
  * Sets the return values in uctx_pool that get passed
@@ -61,24 +100,34 @@ void syscall_set_errno(unsigned int errno)
 void syscall_set_retval1(unsigned int retval)
 {
     // TODO
+    unsigned int curid = get_curid();
+    uctx_pool[curid].regs.ebx = retval;
 }
 
 void syscall_set_retval2(unsigned int retval)
 {
     // TODO
+    unsigned int curid = get_curid();
+    uctx_pool[curid].regs.ecx = retval;
 }
 
 void syscall_set_retval3(unsigned int retval)
 {
     // TODO
+    unsigned int curid = get_curid();
+    uctx_pool[curid].regs.edx = retval;
 }
 
 void syscall_set_retval4(unsigned int retval)
 {
     // TODO
+    unsigned int curid = get_curid();
+    uctx_pool[curid].regs.esi = retval;
 }
 
 void syscall_set_retval5(unsigned int retval)
 {
     // TODO
+    unsigned int curid = get_curid();
+    uctx_pool[curid].regs.edi = retval;
 }
