650c9f823abaac6ef48cc4287d876a35e20c27f7
Melnychuk Anton <am3785@zoo.cs.yale.edu>
2025-02-25 19:24:30 -0500

    TSysCall [Completed]

--- kern/trap/TSyscall/TSyscall.c
+++ kern/trap/TSyscall/TSyscall.c
@@ -15,6 +15,8 @@ static char sys_buf[NUM_IDS][PAGESIZE];
  */
 void sys_puts(void)
 {
+    // @anton-mel: this function is needed for the 
+    // VGA buffer handling over the syscall.
     unsigned int cur_pid;
     unsigned int str_uva, str_len;
     unsigned int remain, cur_pos, nbytes;
@@ -57,6 +59,24 @@ extern uint8_t _binary___obj_user_pingpong_pong_start[];
 extern uint8_t _binary___obj_user_pingpong_ding_start[];
 extern uint8_t _binary___obj_user_fork_test_fork_test_start[];
 
+
+// !NOTE: (defined in user/include/syscall.h)
+// static gcc_inline pid_t sys_spawn(unsigned int elf_id, unsigned int quota)
+// {
+//     int errno;
+//     pid_t pid;
+//
+//     asm volatile ("int %2"                    // IDT syscall handler #2
+//                   : "=a" (errno), "`=b" (pid) // operand %0: output eax to errno + output from EBX to pid
+//                   : "i" (T_SYSCALL),          // operand %1: immediate constant for the interrupt vector
+//                     "a" (SYS_spawn),          // operand %2: placed in EAX before the call
+//                     "b" (elf_id),             // operand %3: placed in EBX before the call
+//                     "c" (quota)               // operand %4: placed in ECX before the call
+//                   : "cc", "memory");          // "cc" means modified + "memory" means may have been altered
+//
+//     return errno ? -1 : pid;
+// }
+
 /**
  * Spawns a new child process.
  * The user level library function sys_spawn (defined in user/include/syscall.h)
@@ -78,7 +98,65 @@ extern uint8_t _binary___obj_user_fork_test_fork_test_start[];
 void sys_spawn(void)
 {
     // TODO
+    // So to implement this function, first we looked at the gcc inline assembly 
+    // provided as a header to the userspace. Thus, we follow the calling conventions
+    // predefined and able to correctly extract the registers values (look above struct).
+    // Given syscall predefined order for getters: EAX, EBX, ECX, EDX, ESI and EDI:
+
+    unsigned int elf_id = syscall_get_arg2(); // elf_id is passed in EBX
+    unsigned int quota = syscall_get_arg3(); // quota is passed in ECX
+    unsigned int child_pid;
+    void *elf_addr = NULL;
+
+    // !NOTE: objects for the ping/pong/ding is imported above.
+    // Map elf_id to the corresponding ELF binary since there 
+    // is no filesystem, so we have to do it manually.
+
+    switch (elf_id) {
+        case 1:
+            elf_addr = _binary___obj_user_pingpong_ping_start;
+            break;
+        case 2:
+            elf_addr = _binary___obj_user_pingpong_pong_start;
+            break;
+        case 3:
+            elf_addr = _binary___obj_user_pingpong_ding_start;
+            break;
+        case 4:
+            elf_addr = _binary___obj_user_fork_test_fork_test_start;
+            break;
+        default:
+            // failure
+            syscall_set_errno(E_INVAL_PID);
+            syscall_set_retval1(NUM_IDS);
+            return;
+    }
+
+    // Create the new process
+    // !NOTE: syscall_set_retval1 fn sets the ret values in uctx_pool
+    // that get passed to the current running prc when returned.
+
+    child_pid = proc_create(elf_addr, quota);
+    if (child_pid == NUM_IDS) {
+        // failure
+        syscall_set_errno(E_INVAL_PID);
+        syscall_set_retval1(NUM_IDS);
+    } else {
+        // success
+        syscall_set_errno(E_SUCC);
+        syscall_set_retval1(child_pid);
+    }
 }
+
+
+// !NOTE: (defined in user/include/syscall.h)
+// static gcc_inline void sys_yield(void)
+// {
+//     asm volatile ("int %0"            // IDT syscall handler #0
+//                   :: "i" (T_SYSCALL), // operand %0: immediate constant for the interrupt vector
+//                      "a" (SYS_yield)  // operand %1: place SYS_yield into EAX before the call
+//                   : "cc", "memory");  // "cc" means modified + "memory" means may have been altered
+// }
 
 /**
  * Yields to another thread/process.
@@ -89,10 +167,42 @@ void sys_spawn(void)
 void sys_yield(void)
 {
     // TODO
+    thread_yield();
+    syscall_set_errno(E_SUCC);
 }
 
+
+// !NOTE: (defined in user/include/syscall.h)
+// static gcc_inline pid_t sys_fork(void)
+// {
+//     int errno;
+//     pid_t pid;
+
+//     asm volatile ("int %2"                   // IDT syscall handler #2
+//                   : "=a" (errno), "=b" (pid) // operand %0: output eax to errno + output from EBX to pid
+//                   : "i" (T_SYSCALL),         // operand %1: immediate constant for the interrupt vector
+//                     "a" (SYS_fork)           // operand %1: immediate constant for the interrupt vector
+//                   : "cc", "memory");         // "cc" means modified + "memory" means may have been altered
+
+//     return pid;
+// }
+
 // Your implementation of fork
-void sys_fork()
+void sys_fork(void)
 {
-    // TODO
+    unsigned int cur_pid = get_curid();
+    unsigned int child_pid;
+
+    // first, define the child given the 
+    // parent current process (elf copied over)
+    child_pid = proc_create((void *)uctx_pool[cur_pid].regs.eip, 0);
+    if (child_pid == NUM_IDS) {
+        // failure
+        syscall_set_errno(E_INVAL_PID);
+        syscall_set_retval1(NUM_IDS);
+    } else {
+        // Success
+        syscall_set_errno(E_SUCC);
+        syscall_set_retval1(child_pid);
+    }
 }
